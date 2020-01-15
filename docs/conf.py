e35b875069be2233460c9c9296439855670eb9b2
adalekin <adalekin@gmail.com>
2020-01-15 11:21:29 +0300

    Bump version: 1.0.3.dev → 1.0.3

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ copyright = "2019, Affo Ltd."
 author = "Affo Ltd."
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.3.dev"
+release = "1.0.3"
 
 
 # -- General configuration ---------------------------------------------------
